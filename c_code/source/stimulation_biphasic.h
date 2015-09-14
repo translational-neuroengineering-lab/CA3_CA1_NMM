ead43b861428bb9d3af65c45575d073ecf98f5bb
Mark Connolly <mark.connolly@emory.edu>
2015-09-14 00:09:46 -0400

    moved

---
+++ c_code/source/stimulation_biphasic.h
@@ -0,0 +1,17 @@
+/*
+ * stimulation_biphasic.h
+
+ *
+ *  Created on: Jun 15, 2015
+ *      Author: mconnolly
+ */
+
+#ifndef STIMULATION_BIPHASIC_H_
+#define STIMULATION_BIPHASIC_H_
+
+
+
+float get_stimulation(float t, double *stimulation_ptr);
+
+#endif /* STIMULATION_BIPHASIC_H_ */
+
